dc44c990febb46d7419dfd5409b80ef0f928fbb0
JSummerfield17 <summerfield.2@wright.edu>
2016-12-05 16:24:09 -0500

    Changed R to F and added Newark_Beta with loop

--- dnplus1.m
+++ dnplus1.m
@@ -1,7 +1,7 @@
-function dnplus1 = dnplus1(Rextnplus1, M, Beta, deltat, dn, ddn, dd2n, C, gamma, K )
+function dnplus1 = dnplus1(F, M, Beta, deltat, dn, ddn, dd2n, C, gamma, K )
 %Find the D_N+1 result
 a=((1/(Beta*deltat^2))*M+(gamma/(Beta*deltat))*C+K)^-1;
-b=a*Rextnplus1;
+b=a*F;
 c=a*(M*((1/(Beta*deltat^2))*dn+(1/(Beta*deltat))*ddn+(1/(2*Beta)-1)*dd2n));
 d=a*(C*((gamma/(Beta*deltat))*dn+(gamma/Beta-1)*ddn+(gamma/Beta-2)*(deltat/2)*dd2n));
 dnplus1=(b+c+d);
