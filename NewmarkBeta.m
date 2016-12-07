110395b9e432dd8b3dcb3d68f31d79f4e5862abf
JSummerfield17 <summerfield.2@wright.edu>
2016-12-07 12:42:01 -0500

    Updated Newmark Beta

--- NewmarkBeta.m
+++ NewmarkBeta.m
@@ -1,40 +1,43 @@
-function [ pos, vel, acc ] = NewmarkBeta( K, M, Beta, gamma, deltat, t )
+function [ pos, vel, acc ] = NewmarkBeta( K, M, F, Beta, gamma, deltat, t )
 %%Finds displacement, velocity, acceleration using the newark beta method
 
+clc
+
 K = full(K);
+K = K(1:30,1:30);
 M = full(M);
+M = M(1:30,1:30);
 C = Damp(K);
 
-time = [0:deltat:t];
+time = (0:deltat:t);
 T = size(time,2);
-R0 = zeros(size(K,1),1);
-R = R0;
-R(15,1) = 100000
+R = full(F(1:30,1));
+R0 = zeros(size(R,1),size(R,2));
 
-dn = zeros(size(K,1),1)
-ddn = zeros(size(K,1),1)
-dd2n = M\R
+dn = zeros(size(K,1),1); %Initial position zero
+ddn = zeros(size(K,1),1); %Initial velocity zero
+size(M);
+size(R);
+dd2n = M\R; %F=ma
 
 
-for i = 1:(T-1)
-    step = time(i)
-    dn = dn(i)
-    ddn = ddn(i)
-    dd2n = dd2n(i)
+for i = 1:T
+    pos(i,:) = dn(:);
+    vel(i,:) = ddn(:);
+    acc(i,:) = dd2n(:);
+    step = time(i);
+    %If still within pluck use R, else use R0
     if step <= t
-        dn(i+1) = dnplus1(R, M, Beta, deltat, dn, ddn, dd2n, C, gamma, K);
+        Dn = dnplus1(R, M, Beta, deltat, dn, ddn, dd2n, C, gamma, K);
     else
-        dn(i+1) = dnplus1(R0, M, Beta, deltat, dn, ddn, dd2n, C, gamma, K);
+        Dn = dnplus1(R0, M, Beta, deltat, dn, ddn, dd2n, C, gamma, K);
     end
-    ddn(i+1) = ddnplus1(Beta,deltat,dnplus1,dn,ddn,dd2n,gamma);
-    dd2n(i+1) = dd2nplus1(Beta, deltat, dn, ddn, dd2n, dnplus1); 
-    pos(i,:,:) = dn(:,:);
-    vel(i,:,:) = ddn(:,:);
-    acc(i,:,:) = dd2n(:,:);
+    Ddn = ddnplus1(Beta,deltat,Dn,dn,ddn,dd2n,gamma);
+    Dd2n = dd2nplus1(Beta, deltat, dn, ddn, dd2n, Dn);
+    dn = real(Dn);
+    ddn = real(Ddn);
+    dd2n = real(Dd2n);
 end
 
-pos
-vel
-acc
 end
 
--- dnplus1.m
+++ dnplus1.m
@@ -4,6 +4,9 @@ a=(1/(Beta*deltat^2))*M+(gamma/(Beta*deltat))*C+K;
 b=Rnplus1;
 c=(M*((1/(Beta*deltat^2))*dn+(1/(Beta*deltat))*ddn+(1/(2*Beta)-1)*dd2n));
 d=(C*((gamma/(Beta*deltat))*dn+(gamma/Beta-1)*ddn+(gamma/Beta-2)*(deltat/2)*dd2n));
+size(b);
+size(c);
+size(d);
 dnplus1t =a\(b+c+d);
 dnplus1 = real(dnplus1t);
 end
